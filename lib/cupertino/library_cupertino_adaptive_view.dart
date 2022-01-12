a16a7b17640f0b729c2e2cbe91299b847c216fd0
PauloPortofolio <pvba02@gmail.com>
2022-01-12 17:41:39 -0500

    6) Library Cupertino + MAterial + Adaptive:
    - Refactoring

--- lib/cupertino/library_cupertino_adaptive_view.dart
+++ lib/cupertino/library_cupertino_adaptive_view.dart
@@ -21,30 +21,25 @@ class LibraryCupertinoAdaptiveView extends StatelessWidget {
                   child: Slider.adaptive(
                       value: 1, onChanged: (_) {}, activeColor: Colors.blue)),
               Material(
-                  child: Slider(
-                      value: 1, onChanged: (_) {}, activeColor: Colors.black)),
+                  child: Slider(value: 1, onChanged: (_) {}, activeColor: Colors.black)),
               CupertinoSwitch(
                   activeColor: CupertinoColors.systemRed, value: true, onChanged: (_) {}),
               Switch.adaptive(activeColor: Colors.blue, value: true, onChanged: (_) {}),
               Switch(activeColor: Colors.black, value: true, onChanged: (_) {}),
               Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Icon(CupertinoIcons.share, color: Colors.red),
-              ),
+                  padding: const EdgeInsets.all(15.0),
+                  child: Icon(CupertinoIcons.share, color: Colors.red)),
               Icon(Icons.adaptive.share, color: Colors.blue),
               Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Icon(Icons.share, color: Colors.black),
-              ),
+                  padding: const EdgeInsets.all(15.0),
+                  child: Icon(Icons.share, color: Colors.black)),
               Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: CupertinoActivityIndicator(),
-              ),
+                  padding: const EdgeInsets.all(15.0),
+                  child: CupertinoActivityIndicator()),
               CircularProgressIndicator.adaptive(backgroundColor: Colors.blue),
               Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: CircularProgressIndicator(backgroundColor: Colors.black),
-              )
+                  padding: const EdgeInsets.all(15.0),
+                  child: CircularProgressIndicator(backgroundColor: Colors.black))
             ],
           ),
         ),
